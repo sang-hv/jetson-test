2aa0ba9b309a7671b7851e3d30692a333bcc9c81
Hoang Van Sang <sang.hv@deha-soft.com>
2026-03-17 09:27:03 +0700

    update lte feature

--- setup-firstboot/scripts/switch-network.sh
+++ setup-firstboot/scripts/switch-network.sh
@@ -18,6 +18,8 @@ CONF_FILE="/etc/device/network.conf"
 case "$MODE" in
     auto|4g|lan|wifi)
         echo "Switching network mode to: $MODE"
+        systemctl restart network-watchdog
+        sleep 5
         
         # Replace the NETWORK_MODE line in the config file
         if grep -q "^NETWORK_MODE=" "$CONF_FILE"; then
@@ -28,7 +30,7 @@ case "$MODE" in
         
         echo "Restarting network-watchdog service..."
         systemctl restart network-watchdog
-        sleep 8
+        sleep 3
         echo "Network mode updated successfully."
         ;;
     *)
